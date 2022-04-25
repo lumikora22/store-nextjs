2dd0969b5a7e627e13c8e84407abd2ca19476b06
Luis Rosales <luisangel@gmail.com>
2022-04-25 16:38:26 -0500

    start project e-commerce

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import "../scss/global.scss"
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
