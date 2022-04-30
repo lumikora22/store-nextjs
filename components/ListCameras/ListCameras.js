05d15bbba9ab47fed0f413a9924868199a02e218
Luis Rosales <luisangel@gmail.com>
2022-04-30 12:29:43 -0500

    change card camera

--- components/ListCameras/ListCameras.js
+++ components/ListCameras/ListCameras.js
@@ -1,60 +1,54 @@
 import {
-	Button,
-	Card,
-	CardActions,
-	CardMedia,
-	Typography,
-} from '@mui/material';
-import CardContent from '@mui/material/CardContent';
-import { map } from 'lodash';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import { map } from "lodash";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 
 const ListCameras = ({ cameras }) => {
-	console.log(cameras);
-	return (
-		<div className='cameras'>
-			<h2>Cámaras</h2>
-			<hr />
-			<div className='list-cameras'>
-				{map(cameras, (camera) => (
-					<Camera camera={camera} />
-				))}
-			</div>
-		</div>
-	);
+  console.log(cameras);
+  return (
+    <div className="cameras">
+      <h2>Cámaras</h2>
+      <hr />
+      <div className="list-cameras">
+        {map(cameras, (camera) => (
+          <Camera camera={camera} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 function Camera({ camera }) {
-	console.log(camera);
-	return (
-		<Card sx={{ maxWidth: 345, maxHeight: 510, m:1 }}>
-			<CardMedia
-				component='img'
-				height='40%'
-				image={camera.poster.url}
-				alt={camera.title}
-			/>
-			<CardContent sx={{ height: 200 }}>
-				<Typography gutterBottom variant='h5' component='div'>
-					{camera.title}
-				</Typography>
-				<Typography variant='body2' color='text.secondary'>
-					{camera.summary}
-          ${camera.price}
-				</Typography>
-			</CardContent>
-			<CardActions sx={{ justifyContent: 'space-around' }}>
-				<Button size='small' variant='contained'>
-					Agregar <LocalGroceryStoreIcon />
-				</Button>
-				<Button size='small'>Ver más</Button>
-				<Button size='small'>
-					<FavoriteBorderIcon />
-				</Button>
-			</CardActions>
-		</Card>
-	);
+  console.log(camera);
+  return (
+    <div className="camera-card">
+      <div className="img-camera-card">
+        <img src={camera.poster.url} alt="" />
+      </div>
+      <div className="content-camera-card">
+        <h3>{camera.title}</h3>
+        <p>{camera.summary}</p>
+      </div>
+      <div className="actions-card">
+        <Button size="small" variant="contained">
+          Agregar <LocalGroceryStoreIcon />
+        </Button>
+        <Button size="small">Ver más</Button>
+{/* 
+        <Button size="small">
+          <FavoriteBorderIcon />
+        </Button> */}
+      </div>
+    </div>
+   
+  );
 }
 
 export default ListCameras;
